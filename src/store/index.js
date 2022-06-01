88a20efd3d0092eae24933f784615ed3c217ea14
karamalqinneh <karamalqinneh@live.com>
2022-06-01 16:46:15 +0300

    completed phase 2

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,18 @@ import { createStore, combineReducers } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 import categoriesReducer from "./categories";
+import cartReducer from "./cart";
+import productsReducer from "./products";
 
 // Combine reducers isn't really necessary when you only have one reducer
 // But ... it's nice to create these with a name (counter in this case) so that your
 // components have easy access to them.
 // And ... 99.99% of all Redux apps will have more than one reducer to 'combine'
-let reducers = combineReducers({ category: categoriesReducer });
+let reducers = combineReducers({
+  category: categoriesReducer,
+  cart: cartReducer,
+  products: productsReducer,
+});
 
 const store = () => {
   return createStore(reducers, composeWithDevTools());
